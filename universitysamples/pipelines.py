0d5c49051a6698635d825a687cf32b825b343eb4
BSY-Development <bes.yamamoto@gmail.com>
2022-01-23 12:05:07 -0300

    feat: cria o setup necessário para a inicialização do banco
    Estaremos utilizando o banco mySQL nesta aplicação. É necessário um arquivo .env para funcionar.

--- universitysamples/pipelines.py
+++ universitysamples/pipelines.py
@@ -5,9 +5,37 @@
 
 
 # useful for handling different item types with a single interface
+import os
 from itemadapter import ItemAdapter
+import mysql.connector
+from dotenv import load_dotenv, find_dotenv
 
+# Aqui é feito a chamada ao arquivo .env para utilizar valores de 
+# conteúdo sensiveis.
+load_dotenv(find_dotenv())
 
 class UniversitysamplesPipeline:
+    # Inicializa o banco
+    def __init__(self):
+        self.con = mysql.connector.connect(
+            host=os.getenv('MYSQL_HOST'),
+            user=os.getenv('MYSQL_USER'),
+            password=os.getenv('MYSQL_PASSWORD'),
+        )
+        self.cur = self.con.cursor()
+        # Cria o banco se não existir
+        self.create_database()
+        # Cria a tabela Users se não existir
+        self.create_table()
+
+    def create_database(self):
+        self.cur.execute("CREATE SCHEMA IF NOT EXISTS University")
+
+    def create_table(self):
+        self.cur.execute("USE University")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS Users (name VARCHAR(100), cpf VARCHAR(11), score FLOAT)")
+
     def process_item(self, item, spider):
+        self.cur.execute("INSERT INTO Users VALUES (%s, %s, %s)", (item['name'], item['cpf'], item['score']))
+        self.con.commit()
         return item
