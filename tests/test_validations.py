984de4a92a41901738584f0712c9902586d9c6e4
BSY-Development <bes.yamamoto@gmail.com>
2022-02-14 00:07:31 -0300

    update: coverage was not working properly, changed to one file test and add coverage screenshot

--- tests/test_validations.py
+++ tests/test_validations.py
@@ -1,4 +1,4 @@
-from validations.validations import format_name
+from validations.validations import format_name, format_cpf, cpf_validate
 import pytest
 
 expected = [
@@ -28,3 +28,12 @@ def test_return_name_formatted_correctly():
   for i in expected:
     assert format_name(i["before"]) == i["after"]
 
+def test_if_cpf_is_valid():
+  assert cpf_validate("222.222.222-22") == False
+  assert cpf_validate("123") == False
+  assert cpf_validate("178.422.117-11") == False
+  assert cpf_validate("01234685744") == True
+
+def test_if_cpf_is_formatted_correcly():
+  assert format_cpf("222.222.222-22") == { "formatted": "22222222222", "is_valid": False }
+  assert format_cpf("012.346.857-44") == { "formatted": "01234685744", "is_valid": True }
